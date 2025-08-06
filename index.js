663ee8452c63bc1741ae2255b74c470fa4ab6bd5
Luisa <luisaalves200@gmail.com>
2025-08-06 13:29:41 -0300

    Commit 2 - Substituir Temp por Query

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr (fatura, pecas) {
+    function getPeca(apresentacao) {
+      return pecas[apresentacao.id];
+    }
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
+    
     const formato = new Intl.NumberFormat("pt-BR",
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = calcularTotalApresentacao(apre, peca);
+          let total = calcularTotalApresentacao(apre);
   
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
-  }
-function calcularTotalApresentacao(apre, peca) {
+  
+function calcularTotalApresentacao(apre) {
   let total = 0;
-  switch (peca.tipo) {
+  switch (getPeca(apre).tipo) {
     case "tragedia":
       total = 40000;
       if (apre.audiencia > 30) {
@@ -43,9 +46,10 @@ function calcularTotalApresentacao(apre, peca) {
       total += 300 * apre.audiencia;
       break;
     default:
-      throw new Error(`Peça desconhecida: ${peca.tipo}`);
+      throw new Error(`Peça desconhecida: ${getPeca(apre).tipo}`);
   }
   return total;
+}
 }
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
